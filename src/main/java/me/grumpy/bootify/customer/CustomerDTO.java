792cf4281774f80584ae6183a4e44a0ae4024ad5
lagardej <4224161+lagardej@users.noreply.github.com>
2025-06-01 14:58:16 +0200

    DTO as record

--- src/main/java/me/grumpy/bootify/customer/CustomerDTO.java
+++ src/main/java/me/grumpy/bootify/customer/CustomerDTO.java
@@ -1,25 +1,13 @@
 package me.grumpy.bootify.customer;
 
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 
 
-@Getter
-@Setter
-public class CustomerDTO {
-
-    private Long id;
-
-    @Size(max = 255)
-    private String firstName;
-
-    @Size(max = 255)
-    private String lastName;
-
-    @Size(max = 255)
-    private String email;
-
-    private Integer age;
-
+public record CustomerDTO(
+        Long id,
+        @Size(max = 255) String firstName,
+        @Size(max = 255) String lastName,
+        @Size(max = 255) String email,
+        Integer age
+) {
 }
